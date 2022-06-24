4bd3b6a1cc656ac32a0631147da58f333bff3a72
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-24 19:56:41 +0200

    24-06-2022 20:01

--- spring-reddit-clone/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
+++ spring-reddit-clone/src/main/java/com/example/springredditclone/SpringRedditCloneApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Import(SwaggerConfiguration.class)
 public class SpringRedditCloneApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRedditCloneApplication.class, args);
 	}
