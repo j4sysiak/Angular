b39b1f54c108397d5545e1ccb9e8b3a901245262
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-24 07:42:15 +0200

    24-06-2022 07:43

--- currency-game-project-with-angular/proj-test/src/app/app.component.ts
+++ currency-game-project-with-angular/proj-test/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proj-test';
+  title = 'hello currency-game';
+  name: string;
+
+  
 }
--- currency-game-project-with-angular/proj-test/src/app/app.module.ts
+++ currency-game-project-with-angular/proj-test/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
