c0e417d7d3b7596b49d5bc3a8a1509c5fb6118ee
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-25 09:35:01 +0200

    25-06-2022 09:34

---
+++ noteIt/noteit-api/src/main/java/com/NoteIt.java
@@ -0,0 +1,12 @@
+package main.java.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteIt {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteIt.class, args);
+	}
+}
