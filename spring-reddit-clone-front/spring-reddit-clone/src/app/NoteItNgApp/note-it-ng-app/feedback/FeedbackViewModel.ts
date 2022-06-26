11352b4d54a8f299196c9b1b512b298b1ccbd172
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-26 08:41:13 +0200

    26-06-2022 08:40

---
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/feedback/FeedbackViewModel.ts
@@ -0,0 +1,8 @@
+
+
+// for the replication object in Backend:  FeedbackViewModel.java
+export interface FeedbackViewModel {
+  name: string;
+  email: string;
+  feedback: string;
+}
--- spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/feedback/feedback.component.ts
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FeedbackViewModel} from "./FeedbackViewModel";
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
+  // ten model odpowiada modelowi w backendzie: FeedbackViewModel.java
+  // musimy zrobić binding tych pól z formularzem html (feedback.component.html)
+  // używamy składni Template Driven Model:  [(ngModel)]="model.xxxx", gdzie xxx może być: name, email, feedback
+  model: FeedbackViewModel = {
+    name: '',
+    email: '',
+    feedback: ''
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  sendFeedback(): void {
+    alert(this.model.name);
+  }
 }
