18b9dbb99eb7a44d8d097aeb22a5b017a8c2d050
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-27 08:36:57 +0200

    27-06-2022 05:35

---
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/model/notebook.ts
@@ -0,0 +1,6 @@
+export interface Notebook {
+  // musi odpowiadać model-view w klasie backendu: NotebookViewModel.java
+  id:string;
+  name:string;
+  nbOfNotes:number;
+}
--- spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/notes.component.ts
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Notebook} from "./model/notebook";
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.getAllNotebooks();
   }
 
+  public getAllNotebooks() {
+    let url =  "http://localhost:8080/api/notebooks/all";
+    this.httpClient.get<Notebook[]>(url).subscribe(
+      response => {
+        this.notebooks = response;
+      },
+      err => {
+        alert("An error has occured");
+      }
+    );
+  }
 }
