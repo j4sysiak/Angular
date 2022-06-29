0a078f8ac73126cc21098daa1537b36a94df9729
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-29 20:07:42 +0200

    29-06-2022 20:06

---
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/note/note.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Note} from "../model/note";
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+  note: Note;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateNote() {
+
+  }
+
+  deleteNote() {
+
+  }
+}
