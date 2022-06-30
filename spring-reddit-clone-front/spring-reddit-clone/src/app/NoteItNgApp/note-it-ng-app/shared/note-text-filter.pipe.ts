3c4392cc82ef54ca42098244c25947c552c2d5d5
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-30 07:29:33 +0200

    30-06-2022 06:36

--- spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/shared/note-text-filter.pipe.ts
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/shared/note-text-filter.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {Note} from "../notes/model/note";
 
 @Pipe({
   name: 'noteTextFilter'
 })
 export class NoteTextFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(notes: Note[], text: string): Note[] {
+    if (text == null || text === "") {
+      return notes;
+    }
+    //text - to wartość wpisywana w polu wyszyukiwarki
+    //wybierze do kolekcji Note[] notatki, które zawierają w polach title lub text wprowadzony do wyszukiwarki text
+    return notes.filter(n => n.title.includes(text) || n.text.includes(text));
   }
 
 }
