3329f0903a526247faef7028f7db977890e99b25
Jacek Sysiak <j4sysiak@gmail.com>
2022-06-30 07:54:35 +0200

    30-06-2022 06:36

--- spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/notes.component.ts
+++ spring-reddit-clone-front/spring-reddit-clone/src/app/NoteItNgApp/note-it-ng-app/notes/notes.component.ts
@@ -13,6 +13,7 @@ export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
   notes: Note[] = [];
   selectedNotebook: Notebook;
+  searchText: string;
 
   constructor(private apiService: ApiService) { }
 
