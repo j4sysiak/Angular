de3e6bb61af2d79e915ef9501e49824461bd9461
Jacek Sysiak <j4sysiak@gmail.com>
2022-09-10 08:06:35 +0200

    10-09-2022 08:06

--- springboot-angular-to-docker/src/main/java/com/example/springbootangulartodocker/controller/EmployeeController.java
+++ springboot-angular-to-docker/src/main/java/com/example/springbootangulartodocker/controller/EmployeeController.java
@@ -1,13 +1,38 @@
 package com.example.springbootangulartodocker.controller;
 
+import com.example.springbootangulartodocker.model.Employee;
+import com.example.springbootangulartodocker.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // wstrzykujemy zależność
+    @Autowired
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    //testowy end point: http://localhost:8080/api/test
     @GetMapping("/test")
     public String get() {
         return "Welcome in SpringbootAngularToDockerApplication";
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeRepository.findAll());
     }
 }
